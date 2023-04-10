0d980a20ccce35865be7f275413d7cdcc7bfcc4a
Alexander Potapov <potapov.av.85@gmail.com>
2023-04-10 19:53:37 +0400

    [feat]: added toggle of sorting history ascending/descending

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,6 +4,7 @@ import { Board } from './Board';
 function Game() {
 	const [history, setHistory] = useState([Array(9).fill(null)]);
 	const [currentMove, setCurrentMove] = useState(0);
+	const [isAscending, setIsAscending] = useState(true);
 	const xIsNext = currentMove % 2 === 0;
 	const currentSquares = history[currentMove];
 
@@ -17,26 +18,40 @@ function Game() {
 		setCurrentMove(nextMove);
 	}
 
-	const moves = history.map((squares, move) => {
-		let description;
-		const isLastMove = history.length - 1 === move;
-		if (isLastMove) {
-			description = 'You are at move #' + move;
-		} else if (move > 0) {
-			description = 'Go to move #' + move;
-		} else {
-			description = 'Go to game start';
-		}
-		return (
-			<li key={move}>
-				{isLastMove ? (
-					<span>{description}</span>
-				) : (
-					<button onClick={() => jumpTo(move)}>{description}</button>
-				)}
-			</li>
-		);
-	});
+	function sortHistory() {
+		return isAscending ? 1 : -1;
+	}
+
+	const moves = history
+		.slice()
+		.sort(sortHistory)
+		.map((squares, index) => {
+			let description;
+			const move = isAscending ? index : history.length - 1 - index;
+			const isLastMove = history.length - 1 === move;
+			if (isLastMove) {
+				description = 'You are at move #' + move;
+			} else if (move > 0) {
+				description = 'Go to move #' + move;
+			} else {
+				description = 'Go to game start';
+			}
+			return (
+				<li key={move}>
+					{isLastMove ? (
+						<span>{description}</span>
+					) : (
+						<button onClick={() => jumpTo(move)}>
+							{description}
+						</button>
+					)}
+				</li>
+			);
+		});
+
+	function handleClickToggleHistory() {
+		setIsAscending((prevValue) => !prevValue);
+	}
 
 	return (
 		<div className="game">
@@ -48,6 +63,16 @@ function Game() {
 				/>
 			</div>
 			<div className="game-info">
+				<div>
+					<span>Sort history: </span>
+					<button onClick={handleClickToggleHistory}>
+						{isAscending ? (
+							<span>&uarr;</span>
+						) : (
+							<span>&darr;</span>
+						)}
+					</button>
+				</div>
 				<ol>{moves}</ol>
 			</div>
 		</div>
